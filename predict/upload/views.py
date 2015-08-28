c82f721541a49fc0fe973703a6c8cbcf6a326e99
dpetralia <dylan.petralia@lamresearch.com>
2015-08-28 11:32:12 -0700

    deleted erf statement/changed sample size
    changed sample for confidence levels

--- predict/upload/views.py
+++ predict/upload/views.py
@@ -20,13 +20,13 @@ def upload(request):
             summary = analyze()
             erf, rpm = find_count()
             inter = 100
-            if rpm > 40 and erf > 20:        
+            if rpm > 60 :        
                 conf = summary.conf_int(.05) 
                 inter = 95
-            elif rpm > 30 and erf > 15:        
+            elif rpm > 40:        
                 conf = summary.conf_int(.1) 
                 inter = 90
-            elif rpm > 20 and erf > 10:        
+            elif rpm > 15:        
                 conf = summary.conf_int(.2) 
                 inter = 80
             else:        
@@ -46,13 +46,13 @@ def auto_post(request):
             summary = analyze()
             erf, rpm = find_count()
             inter = 100
-            if rpm > 40 and erf > 20:        
+            if rpm > 60:        
                 conf = summary.conf_int(.05) 
                 inter = 95
-            elif rpm > 30 and erf > 15:        
+            elif rpm > 40:        
                 conf = summary.conf_int(.1) 
                 inter = 90
-            elif rpm > 20 and erf > 10:        
+            elif rpm > 15:        
                 conf = summary.conf_int(.2) 
                 inter = 80
             else:        
@@ -61,14 +61,14 @@ def auto_post(request):
             subject, from_email, to = 'Part Predictor', 'lampredictor@gmail.com', p.email
             text_content = """
             Based on the mutiple regression tests, we are %d percent certain that this part will come between %.2f and %.2f  days, with an average of %.2f days.
-            If this order is an ERF, it will take between %.2f  and %.2f more days, with an average of %.2f days.
+            
             Based on %d data points.
             Attached is a box plot of all the data we have on the part number specified.
 
             Thanks,
             Lam Part Predictor Team
             """ % (inter, conf[0][0], conf [1][0], summary.params[0], conf[0][1], conf[1],[1], summary.params[1], rpm+erf)
-            html_content = "Based on the mutiple regression tests, we are %d percent certain that this part will come between %.2f and %.2f  days, with an average of %.2f days. <br> If this order is an ERF, it will take between %.2f  and %.2f more days, with an average of %.2f days.<br>Based on %d data points. <br> Attached is a box plot of all the data we have on the part number specified.<p> Thanks,<br>Lam Part Predictor Team"  % (inter, conf[0][0], conf [1][0], summary.params[0], conf[0][1], conf[1],[1], summary.params[1], rpm+erf)
+            html_content = "Based on the mutiple regression tests, we are %d percent certain that this part will come between %.2f and %.2f  days, with an average of %.2f days. <br>Based on %d data points. <br> Attached is a box plot of all the data we have on the part number specified.<p> Thanks,<br>Lam Part Predictor Team"  % (inter, conf[0][0], conf [1][0], summary.params[0], conf[0][1], conf[1],[1], summary.params[1], rpm+erf)
             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
             msg.attach_alternative(html_content, "text/html")
             msg.attach_file(img_path)
